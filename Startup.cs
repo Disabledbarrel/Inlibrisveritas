10527e66694d812144789e84f62dcdecaa78766b
Elin Larsson <elin.elisabeth.larsson@gmail.com>
2020-03-09 11:23:19 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace InlibrisVeritas
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Registrerar services genom dependency injection
+            services.AddControllersWithViews(); // Lägger till MVC-mönstrer
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Tillagda Middleware
+            app.UseStaticFiles();
+            app.UseNodeModules();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Ändrar routing
+                endpoints.MapControllerRoute(
+                     name: "default",
+                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ wwwroot/js/main.js
@@ -0,0 +1,7 @@
+﻿// Navigation
+
+$('nav a#toggle').click(function () {
+    $('ul').slideToggle(200, function () {
+        // Animation complete.
+    });
+});
